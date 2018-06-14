81e1201c9fc210af5a0bd72304987d59313b8b35
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2018-06-14 12:51:58 +0100

    Add new sqlalchemy type Choice which specifies acceptable values

--- hazel_db/types.py
+++ hazel_db/types.py
@@ -36,3 +36,27 @@ class UUID(TypeDecorator):
         if value is None:
             return value
         return uuid.UUID(value)
+
+
+class Choice(TypeDecorator):
+    """Choice offers way of having fixed set of choices for given column. It
+    works with :mod:`enum` in the standard library of Python 3.4+ (the enum34_
+    backported package on PyPi is compatible too for ``< 3.4``).
+
+    :hint: adapted from sqlalchemy_utils ChoiceType.
+    """
+    impl = Integer
+
+    def __init__(self, enum_class, *args, **kwargs):
+        super(Choice, self).__init__(*args, **kwargs)
+        self.enum_class = enum_class
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        return self.enum_class(value).value
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        return self.enum_class(value)
--- tests/test_types.py
+++ tests/test_types.py
@@ -1,5 +1,5 @@
 import pytest
-import uuid
+import enum, uuid
 from sqlalchemy import Column, Integer
 from hazel_db import BASE, types
 
@@ -8,6 +8,18 @@ class DummyEntity(BASE):
     __tablename__ = 'test_entity'
     id = Column(Integer, autoincrement=True, primary_key=True)
     uuid = Column(types.UUID, default=uuid.uuid4)
+
+
+
+class Gender(enum.Enum):
+    MALE = 1
+    FEMALE = 2
+
+
+class DummyPerson(BASE):
+    __tablename__ = 'test_person'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    gender = Column(types.Choice(Gender))
 
 
 class TestUUIDType:
@@ -35,3 +47,30 @@ class TestUUIDType:
 
         found = db.query(DummyEntity).one()
         assert found and isinstance(found.uuid, uuid.UUID)
+
+
+class TestChoiceType:
+
+    def test_can_persist_and_read_ENUM(self, db):
+        get_count = lambda : db.query(DummyPerson).count()
+        assert get_count() == 0
+
+        db.add(DummyPerson(gender=Gender.MALE))
+        db.commit()
+        assert get_count() == 1
+
+        found = db.query(DummyPerson).one()
+        assert found and isinstance(found.gender, Gender)
+        assert found.gender == Gender.MALE
+
+    def test_can_persist_and_read_ENUM_IntValue(self, db):
+        get_count = lambda : db.query(DummyPerson).count()
+        assert get_count() == 0
+
+        db.add(DummyPerson(gender=Gender.FEMALE.value))
+        db.commit()
+        assert get_count() == 1
+
+        found = db.query(DummyPerson).one()
+        assert found and isinstance(found.gender, Gender)
+        assert found.gender == Gender.FEMALE
