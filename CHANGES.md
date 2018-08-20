b9221fafcb5c15b7359bb06c093598c5811fd7c2
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2018-08-20 23:08:35 +0100

    Make project pip installable with setup.py

---
+++ CHANGES.md
@@ -0,0 +1,5 @@
+# CHANGES
+
+## Version: 0.1
+
+- Initial version.
---
+++ setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+import os.path as fs
+
+
+HERE = fs.abspath(fs.dirname(__file__))
+def readfile(name):
+    with open(fs.join(HERE, name)) as f:
+        return f.read()
+
+
+requires = [
+    'sqlalchemy',
+    'zope.sqlalchemy'
+]
+
+tests_require = [
+    'pytest',
+    'pytest-cov'
+]
+
+
+setup(
+    name='hazel_db',
+    version='0.1.dev0',
+    description='A library which eases integrating SQLAlchemy into a project',
+    long_description='\n\n'.join([
+        readfile('CHANGES.md'),
+        readfile('README.md'),
+    ]),
+    author='Hazeltek Solutions',
+    author_email='s.abdulhakeem@hotmail.com',
+    url='https://bitbucket.org/hazeltek-dev/hazel-db',
+    packages=find_packages(where='src', exclude=['tests']),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    python_requires='>=3.5',
+    install_requires=requires,
+    extras_require={
+        'testing': tests_require,
+    },
+    test_suite='tests',
+    zip_safe=False,
+    keywords='hazel sqlalchemy database db',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
+)
