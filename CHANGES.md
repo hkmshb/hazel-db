b457c637d0f2f7c2108443915d3e1175e94fe8a3
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2019-04-14 18:04:00 +0100

    bump version to 0.2.1

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,10 @@
 # CHANGES
 
-## Version: 0.1
+## Version: 0.2.1
+- Changed the `UUIDMixin.id` primary key field type to Unicode.
 
+## Version: 0.2
+- Restructured the code base; merged sparse modules together.
+
+## Version: 0.1
 - Initial version.
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ tests_require = ['black', 'pytest', 'pytest-cov']
 
 setup(
     name='hazel-db',
-    version='0.2.0',
+    version='0.2.1',
     description='A library which eases integrating SQLAlchemy into a project',
     long_description='\n\n'.join(
         [readfile('CHANGES.md'), readfile('README.md')]
